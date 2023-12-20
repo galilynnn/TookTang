dc6a6804ab3160ec34d6357f045282cb796a0c00
Gabriel CHUA <gabriel_chua@tech.gov.sg>
2023-12-21 01:05:01 +0800

    fix azure openai api call

--- app.py
+++ app.py
@@ -7,24 +7,30 @@ client = AzureOpenAI(api_key=st.secrets["AZURE_OPENAI_API_KEY"],
                      azure_endpoint=st.secrets["AZURE_OPENAI_ENDPOINT"],
                      api_version="2023-09-15-preview")
 
-resolution = st.selectbox('Quality?', ('Low', 'High'))
-input_pic = st.camera_input("Photo")
-image_base64 = encode_image(input_pic)
+#######
+st.title("GPT-4V Demo 🖼️")
+system_message = st.text_area("**System Prompt**", "Your task is to identify what is in the given image. Be succinct.")
+input_pic = st.camera_input("**Photo**")
+resolution = st.selectbox('**Image Processing Quality**', ('Low', 'High'))
 
-system_message = st.text_area("System Prompt", "Your task is to identify what is in the given image.")
+if st.button("Run"):
+    if input_pic is None:
+        st.toast('Please take a photo', icon='⚠️')
+    else:
+        image_base64 = encode_image(input_pic)
 
-input_prompt = [{"type": "image_url", "image_url": {
-    "url": f"data:image/jpeg;base64,{image_base64}",
-    "detail": resolution}}]
+        input_prompt = [{"type": "image_url", "image_url": {
+            "url": f"data:image/jpeg;base64,{image_base64}",
+            "detail": resolution}}]
 
-chat_completion = client.chat.completions.create(
-        model="gpt-4v",
-        messages=[
-            {"role": "system", "content": system_message},
-            {"role": "user", "content": input_prompt}],
-        max_tokens=1200,
-        temperature=0)
+        chat_completion = client.chat.completions.create(
+                model="gpt-4v",
+                messages=[
+                    {"role": "system", "content": system_message},
+                    {"role": "user", "content": input_prompt}],
+                max_tokens=1200,
+                temperature=0)
 
-message_content = chat_completion.choices[0].message.content
+        message_content = chat_completion.choices[0].message.content
 
-print(message_content)
+        st.info(message_content)
